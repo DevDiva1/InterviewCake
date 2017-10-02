cbd41ab1d262c6bd49415230b79b810f2cc55f63
Suchi <suchi24@gmail.com>
2017-10-01 20:50:12 -0400

    CountingSort

---
+++ CountingSort/CountingSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSort
+{
+    using System;
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+
+            var result = CountingSort(new [] { 2, 1, 5, 6 }, 6);
+            foreach (var item in result)
+                Console.WriteLine(item);
+            Console.ReadKey();
+        }
+
+        public static int[] CountingSort(int[] input, int max)
+        {
+            var numCount = new int[max + 1];
+            foreach (var item in input)
+            {
+                numCount[item]++;
+            }
+            var sortedArrayIndex = 0;
+            var sortedArray = new int[input.Length];
+            for (var i = 0; i < numCount.Length; i++)
+            {
+                for (var count = 0; count < numCount[i]; count++)
+                {
+                    sortedArray[sortedArrayIndex] = i;
+                    sortedArrayIndex++;
+                }
+            }
+            return sortedArray;
+        }
+
+    }
+}
