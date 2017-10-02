4cd9cca6657e444abb9e311fb0de97efc688dd3a
Suchi <suchi24@gmail.com>
2017-10-02 15:23:42 -0400

    Reverse LinkedList

---
+++ In-Place-Algorithms/ReverseLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace In_Place_Algorithms
+{
+    class ReverseLinkedList
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class LinkedListNode
+        {
+            public int Value { get; set; }
+            public LinkedListNode Next { get; set; }
+
+            public LinkedListNode(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public LinkedListNode Reverse(LinkedListNode root)
+        {
+            var current = root;
+            LinkedListNode previous = null;
+            LinkedListNode nextNode = null;
+
+            while (current != null)
+            {
+                // Copy a pointer to the next element
+                // before we overwrite current.Next
+                nextNode = current.Next;
+
+                // Reverse the 'Next' pointer
+                current.Next = previous;
+
+                // Step forward in the list
+                previous = current;
+                current = nextNode;
+            }
+            return previous;
+        }
+
+    }
+}
