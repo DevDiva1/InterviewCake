00185c28ca17b06a9a4c7f06c56bf55a82660bcd
Suchi <suchi24@gmail.com>
2017-10-05 17:47:17 -0400

    Implemeting Queue with 2 Stacks

---
+++ Arrays/ImplementQueueWith2Stacks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class ImplementQueueWith2Stacks
+    {
+        private Stack<int> _inStack = new Stack<int>();
+        private Stack<int> _outStack = new Stack<int>();
+
+        public void Enqueue(int item)
+        {
+            _inStack.Push(item);
+        }
+
+        public int Dequeue()
+        {
+            if (_outStack.Count == 0)
+            {
+                //Move items from instack to outStack , reversing the order
+                while (_inStack.Count > 0)
+                {
+                    int newestInStackItem = _inStack.Pop();
+                    _outStack.Push(newestInStackItem);
+                }
+            }
+            return _outStack.Pop();
+        }
+    }
+}
