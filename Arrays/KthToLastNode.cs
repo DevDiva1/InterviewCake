c1778f5f2c2ede6e6aed20f842cb73a156f84958
Suchi <suchi24@gmail.com>
2017-10-07 14:28:31 -0400

    Kth to last node in link list

---
+++ Arrays/KthToLastNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arrays;
+
+namespace Arrays
+{
+    class KthToLastNode
+    {
+        public static MainClass.LinkedListNode FindKthToLastNode(MainClass.LinkedListNode root, int kthLastNode)
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (kthLastNode == 1)
+            {
+                return root;
+            }
+
+            var length = 1;
+            MainClass.LinkedListNode traverse = root;
+            while (traverse.Next != null)
+            {
+                length++;
+                traverse = traverse.Next;
+            }
+            traverse = root;
+            for (int i = 0; i < length - kthLastNode; i++)
+            {
+                traverse = traverse.Next;
+            }
+            return traverse;
+        }
+        
+    }
+}
