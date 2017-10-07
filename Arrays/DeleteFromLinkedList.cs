b7184c58bd2e7fbea95fb98ee5d4d9a746b1b707
Suchi <suchi24@gmail.com>
2017-10-06 23:09:24 -0400

    Delete a node from a singly-linked list, ↴ given only a variable pointing to that node.

---
+++ Arrays/DeleteFromLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class LinkedList
+    {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value)
+        {
+            this.value = value;
+        }
+
+        public static void main()
+        {
+            LinkedList a = new LinkedList(1);
+            LinkedList b = new LinkedList(2);
+            LinkedList c = new LinkedList(3);
+
+            DeleteLinkedListNode.DeleteNode(a);
+        }
+    }
+
+    public class DeleteLinkedListNode
+    {
+        public static void DeleteNode(LinkedList b)
+        {
+            var nextNode = b.next;
+            if (nextNode != null)
+            {
+                b.value = nextNode.value;
+                b.next = nextNode.next;
+            }
+            //Deleting last node or linkedList having only 1 node will cause & needs to be carefully handled.
+            else
+            {
+                throw new InvalidOperationException("Can't delete the last node with this method!");
+            }
+        }
+    }
+    
+}
