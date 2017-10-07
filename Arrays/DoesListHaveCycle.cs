a0e8c0f587558bf2e829cc623eb9fe611b2d4a33
Suchi <suchi24@gmail.com>
2017-10-06 23:16:57 -0400

    Does LinkedList have cycle

---
+++ Arrays/DoesListHaveCycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class DoesListHaveCycle
+    {
+        public static bool IsCycle(LinkedListNode<int> firstNode)
+        {
+            var slowRunner = firstNode;
+            var fastRunner = firstNode;
+            // Until we hit the end of the list
+            while (fastRunner != null || fastRunner.Next != null)
+            {
+                slowRunner = slowRunner.Next;
+                fastRunner = fastRunner.Next.Next;
+                // Case: fastRunner is about to "lap" slowRunner
+                if (slowRunner == fastRunner)
+                {
+                    return true;
+                }
+            }
+            // Case: fastRunner hit the end of the list
+            return false;
+        }
+    }
+}
